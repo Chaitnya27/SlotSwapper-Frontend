b5b74dc9ca2f9633ef1c06b81f9f9a88990f9bd7
Chaitnya27 <chaitnyakhedekar02@gmail.com>
2025-11-05 23:53:58 +0530

    fast again

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,89 @@
-import './index.css'
-import './App.css'
-import SignUp from './components/SignUp'
-import { Route, Routes,Navigate } from 'react-router-dom'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
-import Layout from './design/Layout'
-import Home from './components/Home'
-import Eventlist from './components/EventList'
-import Edittask from './components/EditTask'
-import Marketplace from './components/Marketplace.jsx'
-import Outgoingrequests from './components/Outgoingrequests.jsx'
-import Incomingrequests from './components/IncomingRequests.jsx'
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import './index.css';
 
-
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Layout from './design/Layout';
+import Home from './components/Home';
+import Eventlist from './components/EventList';
+import Edittask from './components/EditTask';
+import Marketplace from './components/Marketplace';
+import Outgoingrequests from './components/Outgoingrequests';
+import Incomingrequests from './components/IncomingRequests';
 
 function App() {
- const ProtectedRoute = ({ children }) => {
-  const token = sessionStorage.getItem('token'); // or localStorage
-  return token ? children : <Navigate to="/login" replace />;
-};
+  const ProtectedRoute = ({ children }) => {
+    const token = sessionStorage.getItem('token');
+    return token ? children : <Navigate to="/login" replace />;
+  };
 
   return (
-    <>
     <Routes>
-      <Route element={<Layout/>}>
-      <Route path='/home' element={<Home />} />
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/login' element={<Login/>}/>
-
-      <Route 
-      path='/dashboard'
-      element={
-      <ProtectedRoute>
-        <Dashboard/>
-      </ProtectedRoute>
-      }
-       />
-
-       <Route
-        path="/events" 
-        element={
-        <ProtectedRoute>
-          <Eventlist/>
-          </ProtectedRoute>
-        } />
+      <Route element={<Layout />}>
+        <Route path='/home' element={<Home />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
 
         <Route
-          path="/edit/:id"
+          path='/dashboard'
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/events'
+          element={
+            <ProtectedRoute>
+              <Eventlist />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/edit/:id'
           element={
             <ProtectedRoute>
               <Edittask />
             </ProtectedRoute>
-          }/>
+          }
+        />
 
-           <Route
-      path="/marketplace"
-      element={
-        <ProtectedRoute>
-          <Marketplace />
-        </ProtectedRoute>
-      }
-    />
+        <Route
+          path='/marketplace'
+          element={
+            <ProtectedRoute>
+              <Marketplace />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-      path="/swap/incoming"
-      element={
-        <ProtectedRoute>
-          <Incomingrequests />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/swap/outgoing"
-      element={
-        <ProtectedRoute>
-          <Outgoingrequests />
-        </ProtectedRoute>
-      }
-    />
+        <Route
+          path='/swap/incoming'
+          element={
+            <ProtectedRoute>
+              <Incomingrequests />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/swap/outgoing'
+          element={
+            <ProtectedRoute>
+              <Outgoingrequests />
+            </ProtectedRoute>
+          }
+        />
 
-        
-        <Route path="/" element={<Navigate to="/login" />} />
-        </Route>
-   
+        <Route path='/' element={<Navigate to='/login' />} />
+      </Route>
     </Routes>
-     
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
